ed198e7c125fe56b0b5b39051503d50595aaff02
Youdhbir Singh <engg.tilak@gmail.com>
2020-04-21 10:55:45 +0530

    First commit

---
+++ IntroLibrary/PersonModel.cs
@@ -0,0 +1,19 @@
+namespace IntroLibrary
+{
+    public class PersonModel
+    {
+        public string PersonName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+        public int Age
+        {
+            get; set;
+
+        }
+    }
+}
---
+++ IntroUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel
+            {
+                PersonName = "Youdhbir",
+                LastName = "Singh"
+            };
+
+            System.Console.WriteLine($"My name is {p.PersonName} {p.LastName}");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is first program in VS code");
+        }
+    }
+}
